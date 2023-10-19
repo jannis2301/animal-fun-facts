af756dabf7fb62c759062046e5ebbac5fdedfdea
jannis2301 <jannis.ax@gmx.de>
2023-10-20 00:09:30 +0200

    refactor: added test configs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/animal-fun-facts/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/test/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['clover', 'html'],
+      reportsDirectory: 'target/clover',
+    },
+    reporters: ['junit', 'default'],
+    outputFile: './target/surefire-reports/junit.xml',
+  },
 })
