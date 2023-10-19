26384d7c2a410fc3e00a7ca25c177d218724454a
jannis2301 <jannis.ax@gmx.de>
2023-10-20 00:10:56 +0200

    refactor: added getNextFactIndex & hideFact functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import './index.css'
 const App = () => {
   const [animals, setAnimals] = useState<Array<FishData>>([])
   const [fact, setFact] = useState<string>('')
-  const factRef = useRef<HTMLParagraphElement>(null)
+  const factRef = useRef<HTMLParagraphElement | null>(null)
+  const displayedFacts = useRef<{ [key: number]: number }>({})
 
   useEffect(() => {
     if (fishes) {
@@ -15,17 +16,37 @@ const App = () => {
     }
   }, [])
 
+  const getNextFactIndex = (id: number, factsLength: number) => {
+    if (displayedFacts.current[id] === undefined) {
+      // No facts displayed for this animal yet, start from the beginning
+      displayedFacts.current[id] = 0
+    } else {
+      // Move to the next fact, or wrap around if all facts have been displayed
+      displayedFacts.current[id] =
+        (displayedFacts.current[id] + 1) % factsLength
+    }
+    return displayedFacts.current[id]
+  }
+
   const displayFact = (id: number) => {
     const index = id - 1
-    let facts = animals[index].facts
-    let randomNumber = Math.floor(Math.random() * facts.length)
-    if (randomNumber === index) {
-      randomNumber = index + 1
+    if (index < 0 || index >= animals.length) return
+
+    const facts = animals[index].facts
+    const factIndex = getNextFactIndex(id, facts.length)
+    const factToDisplay = facts[factIndex]
+
+    setFact(factToDisplay)
+
+    if (factRef.current) {
+      factRef.current.classList.add('show-fact')
     }
-    let randomFact = facts[randomNumber]
-    if (factRef.current === null) return
-    factRef.current.style.display = 'block'
-    setFact(randomFact)
+  }
+
+  const hideFact = () => {
+    if (factRef.current) {
+      factRef.current.classList.remove('show-fact')
+    }
   }
 
   return (
@@ -33,8 +54,7 @@ const App = () => {
       <div className="animal-fun-facts">
         <h1 className="title">Click an animal for a fun fact</h1>
         <div className="animals">
-          {animals.map((item) => {
-            const { id, animal, image } = item
+          {animals.map(({ id, animal, image }) => {
             return (
               <img
                 key={id}
@@ -43,7 +63,10 @@ const App = () => {
                 src={image}
                 aria-label={animal}
                 role="button"
-                onClick={() => displayFact(id)}
+                onClick={() => {
+                  displayFact(id)
+                  setTimeout(hideFact, 8000)
+                }}
               />
             )
           })}
