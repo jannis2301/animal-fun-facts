d64196f3e6d759856dca53b69edcc63312207a58
jannis2301 <jannis.ax@gmx.de>
2023-10-23 23:19:45 +0200

    test: added tests cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import App from './App'
 import { render, screen, fireEvent, cleanup } from '@testing-library/react'
-import { describe, it, expect, afterEach } from 'vitest'
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest'
 
 describe('Animal Fun Facts', () => {
+  beforeEach(() => {
+    // tell vitest we use mocked time
+    vi.useFakeTimers()
+  })
+
   afterEach(() => {
     cleanup()
   })
@@ -12,5 +17,51 @@ describe('Animal Fun Facts', () => {
     render(<App />)
     const titleElement = screen.getByText(/Click an animal for a fun fact/i)
     expect(titleElement).toBeInTheDocument()
+  })
+
+  it('displays the initial fact on clicking an animal', () => {
+    render(<App />)
+    const animalImage = screen.getByRole('button', { name: /dolphin/i })
+    fireEvent.click(animalImage)
+
+    const factElement = screen.getByText(
+      /Dolphins have been shown to give distinct names to each other!/i
+    )
+    expect(factElement).toBeInTheDocument()
+  })
+
+  it('cycles through facts when clicking an animal multiple times', () => {
+    render(<App />)
+    const animalImage = screen.getByRole('button', { name: /dolphin/i })
+    fireEvent.click(animalImage)
+
+    const initialFactElement = screen.getByText(
+      /Dolphins have been shown to give distinct names to each other!/i
+    )
+    expect(initialFactElement).toBeInTheDocument()
+
+    fireEvent.click(animalImage)
+
+    const updatedFactElement = screen.getByText(
+      /Dolphins are known to display their own culture!/i
+    )
+    expect(updatedFactElement).toBeInTheDocument()
+  })
+
+  it('hides the fact after 5 seconds', async () => {
+    render(<App />)
+    const animalImage = screen.getByRole('button', { name: /dolphin/i })
+    fireEvent.click(animalImage)
+
+    const factElement = screen.getByText(
+      /Dolphins have been shown to give distinct names to each other!/i
+    )
+    expect(factElement).toBeInTheDocument()
+    expect(factElement).toHaveClass('show-fact')
+
+    // Fast-forward time by 5 seconds
+    vi.advanceTimersByTime(5000)
+
+    expect(factElement).not.toHaveClass('show-fact')
   })
 })
